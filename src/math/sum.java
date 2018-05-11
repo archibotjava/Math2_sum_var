2882c5d294d035d683ad17db0be883b628aaf82d
Archi <archibk32@gmail.com>
2018-05-11 15:18:55 +0300

    Second repo

---
+++ src/math/sum.java
@@ -0,0 +1,23 @@
+package math;
+import java.io.*;
+import java.util.*;
+
+public class sum {
+    int a=6;
+    int b=9;
+
+
+    void sum(){
+        int c = a + b;
+        System.out.println("Result = "+c);
+
+    }
+
+    public static void main(String[] args) {
+
+        sum res = new sum();
+
+        res.sum();
+
+    }
+}
